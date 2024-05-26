988029d56dcc168ba5926a9d157c28d77163f343
Awaaz <awaaz@Awaazs-MacBook-Pro.local>
2024-05-26 14:26:10 +0530

    Spiral Pattern Creation Completed

--- src/code.js
+++ src/code.js
@@ -1,32 +1,48 @@
-figma.showUI(__html__, { width: 280, height: 500 });
-
 figma.ui.onmessage = msg => {
-  if (msg.type === 'create-grid') {
-    const nodes = [];
-    const emojis = msg.emojis; // Array of selected emojis
-    const gridRows = msg.gridRows; // Number of rows in the grid
-    const gridCols = msg.gridCols; // Number of columns in the grid
-    const spacing = msg.spacing; // Spacing between emojis
-
-    for (let row = 0; row < gridRows; row++) {
-      for (let col = 0; col < gridCols; col++) {
-        const emojiIndex = (row * gridCols + col) % emojis.length;
-        const node = figma.createNodeFromSvg(emojis[emojiIndex]);
-        const group = figma.group(node.children, figma.currentPage);
-        node.remove();
-
-        group.x = col * (group.width + spacing);
-        group.y = row * (group.height + spacing);
-
-        nodes.push(group);
+    if (msg.type === 'create-grid') {
+      const nodes = [];
+      const emojis = msg.emojis; // Array of selected emojis
+      const turns = 5; // Number of turns in each spiral
+      const spacing = 1200; // Spacing between emojis
+      const spiralCount = 15; // Number of spirals
+      const minScale = 0.5; // Minimum scale of the emoji
+      const maxScale = 6.0; // Maximum scale of the emoji
+  
+      for (let s = 0; s < spiralCount; s++) {
+        let angle = (s * 2 * Math.PI) / spiralCount; // Initial angle for each spiral
+        let radius = 0;
+  
+        for (let i = 0; i < turns * emojis.length; i++) {
+          const emojiIndex = i % emojis.length;
+          const node = figma.createNodeFromSvg(emojis[emojiIndex]);
+          const group = figma.group(node.children, figma.currentPage);
+          node.remove();
+  
+          const x = Math.cos(angle) * radius;
+          const y = Math.sin(angle) * radius;
+  
+          // Calculate scaling factor based on position in the spiral
+          const scaleFactor = minScale + (maxScale - minScale) * (i / (turns * emojis.length));
+  
+          // Apply scaling to the group
+          group.rescale(scaleFactor);
+  
+          group.x = x + figma.viewport.center.x;
+          group.y = y + figma.viewport.center.y;
+  
+          nodes.push(group);
+  
+          angle += spacing / (2 * Math.PI * radius || 1); // increment angle based on the circumference of the current radius
+          radius += spacing / (2 * Math.PI); // increment radius slightly with each emoji
+        }
       }
+  
+      if (nodes.length > 0) {
+        figma.currentPage.selection = nodes;
+        figma.viewport.scrollAndZoomIntoView(nodes);
+      }
+      figma.ui.postMessage({ type: 'SCALING_MULTI_SPIRAL_CREATION_SUCCESS' });
     }
-
-    if (nodes.length > 0) {
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
-    }
-
-    figma.ui.postMessage({ type: 'GRID_CREATION_SUCCESS' });
-  }
-};
+  };
+  
+figma.showUI(__html__, { width: 280, height: 500 });
--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,4 @@
-import './ui.css'
+import './ui.css';
 import _ from "lodash";
 import $ from "jquery";
 
@@ -7,11 +7,11 @@ let selectedEmojis = []; // Array to store selected emoji SVGs
 
 $(document).ready(function () {
     fetchEmojiUnicodes();
-    setupEventListeners(); // Setting up additional event listeners
+    setupEventListeners();
 });
 
 // On clicking tabs
-$(document).on("click","ul.tabs li", function(){
+$(document).on("click", "ul.tabs li", function () {
     const category = $(this).attr('data-tab');
     $('ul.tabs li').removeClass('current');
     $('.tab-content').removeClass('current');
@@ -21,25 +21,24 @@ $(document).on("click","ul.tabs li", function(){
 });
 
 // Adding shadow on scroll
-$('#emoji-container').on('scroll', function() {
+$('#emoji-container').on('scroll', function () {
     if (!$('#emoji-container').scrollTop()) {
-        $('.container').removeClass('shadow')
+        $('.container').removeClass('shadow');
     } else {
-        $('.container').addClass('shadow')
+        $('.container').addClass('shadow');
     }
-})
+});
 
 // Function to setup additional event listeners
 const setupEventListeners = () => {
-    // Event listener for grid generation button
     document.getElementById('create-grid-button').addEventListener('click', () => {
         if (selectedEmojis.length > 0) {
             parent.postMessage({
                 pluginMessage: {
                     type: 'create-grid',
                     emojis: selectedEmojis,
-                    gridRows: 5, // Set your desired grid size or make it configurable
-                    gridCols: 5,
+                    gridRows: 10, // Set your desired grid size or make it configurable
+                    gridCols: 10,
                     spacing: 10 // Set your desired spacing or make it configurable
                 }
             }, '*');
@@ -48,18 +47,17 @@ const setupEventListeners = () => {
         }
     });
 
-    // Event listener for clear selection button
     document.getElementById('clear-selection-button').addEventListener('click', () => {
+        $('.selected-emoji').removeClass('selected-emoji');
         selectedEmojis = [];
-        updateSelectedEmojisDisplay();
     });
-}
+};
 
-// Listing all the Emojis from the unicode list onto the view
+// Populate Emojis
 const populateEmojis = (list) => {
     let emojiUnicodes = '';
-    for(let i=0; i<list.length; i++) {
-        if(!emojiUnicodes.includes(list[i].char)) {
+    for (let i = 0; i < list.length; i++) {
+        if (!emojiUnicodes.includes(list[i].char)) {
             emojiUnicodes += list[i].char;
         }
     }
@@ -75,59 +73,61 @@ const populateEmojis = (list) => {
     let imgs = document.getElementsByTagName("img");
     for (let i = 0; i < imgs.length; i++) {
         let src = imgs[i].src;
-        imgs[i].onclick = function() {fetchImg(src)};
+        imgs[i].onclick = function () {
+            fetchImg(this, src);
+        };
     }
-}
+};
 
-/* Fetching the unicodelist from
- * https://github.com/amio/emoji.json
- */
+// Fetch Emoji Unicodes
 const fetchEmojiUnicodes = () => {
-    fetch("https://unpkg.com/emoji.json@12.1.0/emoji.json")
-    .then(res => res.json())
-    .then((emojiList) => {
-        emojiUnicodeList = emojiList;
-        emojiUnicodeList = _.groupBy(emojiList, (emoji) => {
-            return emoji.category.substr(0, emoji.category.indexOf('(')).trim();
+    fetch("https://unpkg.com/emoji.json@13.1.0/emoji.json")
+        .then(res => res.json())
+        .then((emojiList) => {
+            emojiUnicodeList = emojiList;
+            emojiUnicodeList = _.groupBy(emojiList, (emoji) => {
+                return emoji.category.substr(0, emoji.category.indexOf('(')).trim();
+            });
+
+            for (const key in emojiUnicodeList) {
+                $('#tab-list').append('<li class="tab-link" data-tab="' + key + '">' + key + '</li>');
+            }
+            $('.tab-link').eq(0).click();
+        })
+        .catch(() => {
+            console.log('There was an issue while fetching the emoji list');
+            document.getElementById('emoji-container').setAttribute('style', 'display:none');
+            document.getElementById('error').setAttribute('style', 'display:flex');
         });
+};
 
-        // Adding appropriate category tabs
-        for (const key in emojiUnicodeList) {
-            $('#tab-list').append('<li class="tab-link" data-tab="' + key +'">' + key + '</li>');
-        }
-        $('.tab-link').eq(0).click();
+// Toggle Emoji Selection
+const toggleEmojiSelection = (emojiElement, emojiSVG) => {
+    const isSelected = $(emojiElement).hasClass('selected-emoji');
+    if (isSelected) {
+        $(emojiElement).removeClass('selected-emoji');
+        selectedEmojis = selectedEmojis.filter(e => e !== emojiSVG);
+    } else {
+        $(emojiElement).addClass('selected-emoji');
+        selectedEmojis.push(emojiSVG);
+    }
+};
 
-    })
-    .catch(() => {
-        console.log('There was an issue while fetching the emoji list');
-        document.getElementById('emoji-container').setAttribute('style', 'display:none');
-        document.getElementById('error').setAttribute('style', 'display:flex');
-    });
-}
-
-// Function to update UI with selected emojis
-const updateSelectedEmojisDisplay = () => {
-    const container = document.getElementById('selected-emojis-container');
-    container.innerHTML = selectedEmojis.join(' '); // Display selected emojis
-}
-
-// Fetching svg code of selected Emoji and adding to the selection
-const fetchImg = url => {
+// Fetch SVG code of clicked Emoji and toggle its selection
+const fetchImg = (emojiElement, url) => {
     fetch(url).then(r => r.arrayBuffer()).then(buff => {
-        let blob = new Blob([new Uint8Array(buff)], {type: "image/svg+xml"});
+        let blob = new Blob([new Uint8Array(buff)], { type: "image/svg+xml" });
         const reader = new FileReader();
         reader.onload = () => {
-            selectedEmojis.push(reader.result);
-            updateSelectedEmojisDisplay(); // Update UI with selected emojis
+            toggleEmojiSelection(emojiElement, reader.result);
         };
         reader.readAsText(blob);
     });
-}
+};
 
 // Function to receive events from Figma
 onmessage = e => {
     if (!e.data) return;
-
     const data = e.data.pluginMessage.data;
     const type = e.data.pluginMessage.type;
 };
